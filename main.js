ee801d790276eb165e718d7c49bd014785e35544
Manuel <manuelaccode@gmail.com>
2025-06-16 20:30:30 -0700

    Ahora se genera una cantidad de tarjetas igual a los descuentos que tiene la empresa con ID MyID (variable que se puede cambiar en main.js)

---
+++ main.js
@@ -0,0 +1,36 @@
+import {createClient} from 
+'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseUrl = 'https://mmcgruaofxzxpecdhvpl.supabase.co'
+const supabaseKey= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tY2dydWFvZnh6eHBlY2RodnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3ODA2OTgsImV4cCI6MjA2NTM1NjY5OH0.KXigWjfRLrBJFGr_9LFYKwtCucy6AxkF5BNziULUASQ'
+
+const supabase=createClient(supabaseUrl,supabaseKey)
+
+window.onload= obtenerDescuentos;
+
+
+var myid =1;
+
+async function obtenerDescuentos(){
+
+
+
+   let { data, error } = await supabase
+  .rpc('my_discounts', {
+    myid
+  })
+if (error) console.error(error)
+
+    const lista=
+document.getElementById("cardShow")
+lista.innerHTML=''
+
+data.forEach(descuento => {
+    const div =document.createElement('div')
+    div.classList.add('card');
+    
+    lista.appendChild(div);
+});
+
+}
+
